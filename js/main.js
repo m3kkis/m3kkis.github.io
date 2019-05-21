dae6d73c927141608e408556e96569f057fc3a7a
Daniel Mekker <daniel.mekker@gmail.com>
2019-05-21 01:42:47 -0400

    added base layout of the page, added menu navigation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 function Init(){
-    
+
+    $(".nav_menu_btn").off();
+    $(".nav_menu_btn").on("mousedown touchstart",function(){
+        if($(this).hasClass("is-active"))
+        {
+            $(this).removeClass("is-active");
+            $(".navigation_menu_open").addClass("hide-me");
+            $(".logo_menu_container").removeClass("hide-me");
+        }
+        else
+        {
+            $(this).addClass("is-active");
+            $(".navigation_menu_open").removeClass("hide-me");
+            $(".logo_menu_container").addClass("hide-me");
+        }
+        
+    });
+
 }
