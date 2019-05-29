12592cca3d16aaeb5bc8ddea2bc635a6108868af
Daniel Mekker <daniel.mekker@gmail.com>
2019-05-29 19:58:35 -0400

    Added email, text spelling fix, fixed all nav links

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 
+    /* INITIALIZE LIBRARY */
     AOS.init();
     
+    /* OPEN AND CLOSE EVENT FOR MENU */
     $(".nav_menu_btn").off();
-    $(".nav_menu_btn").on("mousedown touchstart",function(){
+    $(".nav_menu_btn").on("click",function(){
         if($(this).hasClass("is-active"))
         {
             $(this).removeClass("is-active");
@@ -18,5 +20,41 @@ $(document).ready(function(){
         }
         
     });
+
+
+    /* CLICK EVENTS FOR MENU */
+    $(".nav_link").off();
+    $(".nav_link").on("click",function(){
+        $(".nav_menu_btn").removeClass("is-active");
+        $(".navigation_menu_open").addClass("hide-me");
+        $(".logo_menu_container").removeClass("hide-me");
+    });
+
+    /* EMAIL VALIDATION KEY EVENT */
+    $(".email_txtbox").on("keyup",function(){
+
+        if( $(this).val() == "")
+        {
+            $(".email_validation_front").text("");
+        }
+        else if( isEmailValid( $(this).val() ) )
+        {
+            $(".email_validation_front").text("Email is valid!");
+            $(".email_validation_front").css("color","green");
+            $('.message_btn').prop('disabled', false);
+        }
+        else
+        {
+            $(".email_validation_front").text("Email is invalid!");
+            $(".email_validation_front").css("color","red");
+            $('.message_btn').prop('disabled', false);
+        }
+    })
+
+    /* VERIFIES IF EMAIL IS CORRECT AND RETURN BOOL */
+    function isEmailValid(email) {
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
+    }
 
 });
