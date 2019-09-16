709ad893a677ff62ce57195d747a912e47595ad2
daniel <daniel@buybackbooth.com>
2019-09-16 17:59:37 -0400

    fixed text, spacings, added nav menu, font size, little details here and there

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
     new GitHubCalendar(".calendar", "m3kkis");
+
+    $(".nav_menu_btn").off();
+    $(".nav_menu_btn").on("click",function(){
+        if($(this).hasClass("is-active"))
+        {
+            $(this).removeClass("is-active");
+        }
+        else
+        {
+            $(this).addClass("is-active");
+        }
+        
+    });
 });
