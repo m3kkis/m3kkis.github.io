ced296041545c8b514b3228ebb7bdc925ce0f521
Daniel Mekker <daniel.mekker@gmail.com>
2020-03-21 20:49:42 -0400

    added english,french,russian translations

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
 $(document).ready(function(){
+
+    var _PROJ_DATA;
+    var _TRANS_DATA;
+    
     $.ajax({
         url: "js/projects.json",
         dataType: "json",
         success: function(data){
-            console.log("json load success");
-            appendProjectsList(data);
+            console.log("json projects load success");
+            _PROJ_DATA = data;
+            appendProjectsList(_PROJ_DATA,"en");
             twemoji.parse(document.body);
         },
         error: function(e){
-            console.log("failed to load json");
+            console.log("failed to load projects json");
+        }
+    });
+
+    $.ajax({
+        url: "js/translation.json",
+        dataType: "json",
+        success: function(data){
+            console.log("json translation load success");
+            _TRANS_DATA = data;
+            translate(_TRANS_DATA, "en");
+        },
+        error: function(e){
+            console.log("failed to load translation json");
         }
     });
 
@@ -20,6 +38,12 @@ $(document).ready(function(){
             $(document.documentElement).attr('data-theme', 'dark');
         }    
     });
+
+    $(".lang").on('click',function(){
+        var me = this;
+        appendProjectsList(_PROJ_DATA, $(me).attr("data-lang") );
+        translate(_TRANS_DATA, $(me).attr("data-lang"));
+    })
 
     $(".filter-tag").on("click",function(){
         var me = this;
@@ -68,7 +92,8 @@ $(document).ready(function(){
         });
     }
 
-    function appendProjectsList(projectsList){
+    function appendProjectsList(projectsList, lang){
+        $("#projects-list").html('');
         var sHtml = "";
         for(var i = 0; i < projectsList.length; i++)
         {
@@ -78,24 +103,39 @@ $(document).ready(function(){
 
                 for(var j = 0; j < projectsList[i].tags.length; j++)
                 {
-                    /*
-                        html
-                        css
-                        javascript
-                        python
-                        shell
-                    */
-
                    sTags += '<div class="code-tags tag-'+projectsList[i].tags[j]+'">'+projectsList[i].tags[j].toUpperCase()+'</div>';
-
                 }
             }
             
             var prg = ((projectsList[i].progress == 'complete') ? "✅" : (projectsList[i].progress == 'incomplete') ? "🔨" : (projectsList[i].progress == 'inprogress') ? "🛠" : "" );
             
-            sHtml += '<li class=""><div><a href="'+projectsList[i].link+'">'+ prg +projectsList[i].name+'</a>'+sTags+'<br>'+projectsList[i].description+'</div></li>';
+            if(lang == 'fr')
+            {
+                sHtml += '<li class=""><div><a href="'+projectsList[i].link+'">'+ prg +projectsList[i].name+'</a>'+sTags+'<br>'+projectsList[i].description_fr+'</div></li>';
+            }
+            else if(lang == 'ru')
+            {
+                sHtml += '<li class=""><div><a href="'+projectsList[i].link+'">'+ prg +projectsList[i].name+'</a>'+sTags+'<br>'+projectsList[i].description_ru+'</div></li>';
+            }
+            else
+            {
+                sHtml += '<li class=""><div><a href="'+projectsList[i].link+'">'+ prg +projectsList[i].name+'</a>'+sTags+'<br>'+projectsList[i].description_en+'</div></li>';
+            }
+            
         }
         $("#projects-list").append(sHtml);
+    }
+
+    function translate(translations, lang){
+        for(var i = 0; i < translations.length; i++)
+        {
+            if(translations[i].txtlang == lang)
+            {
+                Object.keys(translations[i]).forEach( function(key) { 
+                    $("#"+key).text(translations[i][key])
+                });
+            }
+        }
     }
 
 });
